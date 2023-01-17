b778f8bd463dff906c83c4f856029b120123175e
Alif Aiman <alifaiman.edu@gmail.com>
2023-01-17 16:25:01 +0800

    Test

---
+++ Classical Encryptions/Elliptical Curve/Simple test.py
@@ -0,0 +1,2 @@
+# https://github.com/nakov/Practical-Cryptography-for-Developers-Book/blob/master/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc.md
+
---
+++ Classical Encryptions/Factorization of huge numbers.py
@@ -0,0 +1,80 @@
+import random
+import time
+import pandas as pd
+import numpy as np
+from random import randint as rand
+from matplotlib import pyplot as plt
+
+t = [] #Store Elapsed Time
+k = [] #Store lenght of integer
+integer=[] #Store Integers
+intlen = 20
+
+
+# Get Prime Factors of the number
+def get_primefactors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+try:
+    j = 0
+    for i in range(1, intlen + 1):
+        print("--------------------------------------------------------------")
+        # Get random Integer
+        random.seed(132514247) #seed random number generator
+        integer.append(rand(pow(10, i - 1), pow(10, i)))
+
+        # Start clock time
+        start = time.time()
+        print('Iteration number', i)
+        print('Number to be Factorized :', integer[j])
+
+        # Factorization using get_primefactor function
+        print(get_primefactors(integer[j]))
+
+        # Get Final time to compute the script
+        end = time.time() - start
+        t.append(end)
+        k.append(i)
+        print(end, 'seconds')
+
+        # plots graph
+        plt.plot(k, t, '-o')
+        j += 1
+
+    plt.title("Factorization of Integers Using Brute Force Method")
+    plt.suptitle('Seed=132514247')
+    plt.ylabel('time,s')
+    plt.xlabel('no. of digits')
+    plt.xticks(np.arange(min(k), max(k) + 1, 1))
+    plt.show()
+
+except KeyboardInterrupt: #manually interrupt code incase of long compute times
+    plt.title("Factorization of Integers Using Brute Force Method")
+    plt.suptitle('Seed=132514247')
+    plt.ylabel('time,s')
+    plt.xlabel('no. of digits')
+    plt.xticks(np.arange(min(k), max(k) + 1, 1))
+    plt.show()
+
+#making pandas dataframe
+data = {
+    'Integer':integer,
+    'time': t,
+    'no of digits': k
+}
+#Saving Data to Excel Sheet
+df1 = pd.DataFrame(data)
+writer = pd.ExcelWriter(r'C:\Users\alifa\OneDrive\Documents\Universitas Malaya\FYP\Quantum\Seeded CSC\Seeded Data.xlsx',mode='a')
+df1.to_excel(writer, index=False, sheet_name='10')
+writer.save()
